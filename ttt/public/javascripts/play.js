532af1b773741490ac9a7a8977b4c31374c75179
Ubuntu <ubuntu@instance.cloud.compas.cs.stonybrook.edu>
2019-02-18 06:56:47 +0000

    Sending and receiving HTTP requests setup.

--- ttt/public/javascripts/play.js
+++ ttt/public/javascripts/play.js
@@ -1,6 +1,21 @@
-document.querySelectorAll('.cell')
-.forEach(e => e.addEventListener("click", function() {
-    // Here, `this` refers to the element the event was hooked on
-    console.log("clicked");
-    
-}));
+$('.cell')
+.click(function() {
+    var gameState = "";
+    $('.char').each(function() {
+        gameState += $(this).text();
+    });
+    console.log(gameState);
+    var json = {gameState: gameState};
+    json = JSON.stringify(json);
+    $.post("/ttt/play", json);
+    /*$.ajax({
+        url: '130.245.168.173:3000/ttt/play',
+        type: 'POST',
+        data: json,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+       	success: function(msg) {
+	    console.log("request sent");
+        }
+    });*/
+});
